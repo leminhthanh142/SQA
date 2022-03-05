691d10641b2e4d33eafe776854270ad3859405b5
leminhthanh142 <thanhle12345789@gmail.com>
2022-03-06 00:17:50 +0700

    update, add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+
 function App() {
-  return <div className="App">SQA</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
