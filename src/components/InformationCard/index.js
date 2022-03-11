19c8106a1ad908796b4157be56057744968f0654
leminhthanh142 <thanhle12345789@gmail.com>
2022-03-11 23:51:22 +0700

    update style

--- src/components/InformationCard/index.js
+++ src/components/InformationCard/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { Card, CardContent, Typography, Box } from '@mui/material';
 import PropTypes from 'prop-types';
+import { createStyles, makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    container: {
+      transition: 'all 0.6s',
+      padding: '47px 46px 36px',
+      border: '1px solid',
+      borderColor: '#e6dad8',
+      '&:hover': {
+        borderColor: '#ffffff',
+        boxShadow: '-5px 15px 25px 12px rgba(255,146,146,0.1)'
+      }
+    }
+  })
+);
 
 export const InformationCard = ({ icon, title, description }) => {
+  const classes = useStyles();
   return (
-    <Card>
-      <Box>{icon}</Box>
+    <Card elevation={0} className={classes.container} sx={{ borderRadius: '10px' }}>
+      <Box display={'flex'} alignItems={'center'} justifyContent={'center'}>
+        {icon}
+      </Box>
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant="h5" component="div" align={'center'}>
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" align={'center'}>
           {description}
         </Typography>
       </CardContent>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,19 +3,22 @@ import { Header } from '../components/Header';
 import { MainLayout } from '../components/layout/MainLayout';
 import { informationCard } from '../dummyData';
 import { InformationCard } from '../components/InformationCard';
+import { Stack } from '@mui/material';
 
 export const HomePage = () => {
   return (
     <MainLayout>
       <Header />
-      {informationCard.map((card) => (
-        <InformationCard
-          key={card.id}
-          icon={card.icon}
-          title={card.title}
-          description={card.description}
-        />
-      ))}
+      <Stack direction={'row'} spacing={3}>
+        {informationCard.map((card) => (
+          <InformationCard
+            key={card.id}
+            icon={card.icon}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </Stack>
     </MainLayout>
   );
 };
