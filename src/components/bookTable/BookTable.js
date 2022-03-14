f76eedf2d9b5b97feef9fac6c763b6d27358990e
Dara21032001 <vuthihue21032001@gmail.com>
2022-03-14 22:18:11 +0700

    Hue update BookTable

--- src/components/bookTable/BookTable.js
+++ src/components/bookTable/BookTable.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Stack } from '@mui/material';
+import { Stack, Box, Typography, Button, TextField, Select } from '@mui/material';
 import { createStyles, makeStyles } from '@mui/styles';
-import { styled } from '@mui/system';
+import { fontFamily, height, styled } from '@mui/system';
 
 export const BookTable = () => {
   const [guestNo, setGuestNo] = useState('');
@@ -39,31 +39,96 @@ export const BookTable = () => {
 
   // may cai div cac thu h2, p thi c dung cuar thu vien het di Box + Typography
   return (
-    <Stack direction={'row'}>
+    <Stack direction={'row'} mt={15}>
       <div>
         <img src="/images/bookTable.png" />
       </div>
-      <Stack>
-        <h2>Book a table</h2>
-        <p>
+      <Box
+        ml={19}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          width: '49.5'
+        }}>
+        <Typography
+          variant="h3"
+          sx={{
+            fontFamily: '"Mitr",sans-serif',
+            fontSize: '5.5',
+            display: 'block',
+            marginBottom: '2.75',
+            color: '#4d312c',
+            fontWeight: 'bold'
+          }}>
+          Book a table
+        </Typography>
+        <Box sx={{ typography: 'subtitle1' }}>
           Making a reservation at Délicious restaurant is easy and takes just a couple of minutes.
-        </p>
-        <Stack direction={'row'}>
-          <input type="text" name="guestNo" value={guestNo} onChange={handleChangeGuestNo} />
-          <input type="text" name="tableNo" value={tableNo} onChange={handleChangeTableNo} />
+        </Box>
+        <Stack direction={'row'} mb={3} mt={3}>
+          <TextField
+            sx={{
+              mr: 2.5
+            }}
+            size={'small'}
+            placeholder="Number Of Guest"
+            name="guestNo"
+            value={guestNo}
+            onChange={handleChangeGuestNo}
+          />
+          <TextField
+            size={'small'}
+            placeholder={'Number Of Table'}
+            type="text"
+            name="tableNo"
+            value={tableNo}
+            onChange={handleChangeTableNo}
+          />
         </Stack>
-        <Stack direction={'row'}>
-          <input type="date" name="date" value={date} onChange={handleChangeDate} />
-          <input
-            type="number"
+        <Stack direction={'row'} mb={3}>
+          <TextField
+            sx={{ width: 222, mr: 2.5 }}
+            size={'small'}
+            type="date"
+            name="date"
+            value={date}
+            onChange={handleChangeDate}
+          />
+          <TextField
+            sx={{ width: 222 }}
+            size={'small'}
             name="tableType"
+            placeholder={'Table Type'}
             value={tableType}
             onChange={handleChangeTableType}
           />
         </Stack>
-        <input type="text" name="phoneNo" value={phoneNo} onChange={handleChangePhoneNo} />
-        <button>Send Request</button>
-      </Stack>
+        <TextField
+          sx={{ width: 462, mb: 3 }}
+          placeholder={'Phone No.'}
+          name="phoneNo"
+          value={phoneNo}
+          onChange={handleChangePhoneNo}
+        />
+        <TextField
+          sx={{ width: 462, mb: 3 }}
+          multiline
+          rows={3}
+          placeholder={'Message'}
+          name="message"
+          value={message}
+          onChange={handleChangeMessage}
+        />
+        <Button
+          sx={{
+            width: 462,
+            background: '#FE5F41'
+          }}
+          variant="contained">
+          Send Request
+        </Button>
+      </Box>
     </Stack>
   );
 };
