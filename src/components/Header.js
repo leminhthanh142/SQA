7b4dd142bbfef5974fcad04a7c41350ea661b0e4
leminhthanh142 <thanhle12345789@gmail.com>
2022-03-15 23:58:51 +0700

    update header, global style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { Box } from '@mui/material';
-import { createStyles, makeStyles } from '@mui/styles';
+import React, { useEffect, useState } from 'react';
+import { Box, Stack } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom';
 import { CustomButton } from './CustomButton';
@@ -8,35 +7,59 @@ import { styled } from '@mui/system';
 
 export const Header = () => {
   const [isShrink, setIsShrink] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      onScroll(window.pageYOffset);
+    });
+    return () => {
+      window.removeEventListener('scroll', () => onScroll(window.pageYOffset));
+    };
+  }, []);
+
+  const onScroll = (height) => {
+    if (height >= 250) {
+      setIsShrink(true);
+      return;
+    }
+    setIsShrink(false);
+  };
+
   return (
-    <header>
-      <Container>
-        <Box display={'flex'} alignItems={'center'}>
-          <Box mr={3}>
-            <img src="/images/sqa-logo.webp" alt="logo" />
-          </Box>
-          <Link to={'/'}>Home</Link>
-          <Link to={'/menu'}>Menu</Link>
-          <Link to={'/contact'}>Contact</Link>
-          <Link to={'/booking'}>Book a Table</Link>
+    <Container isShrink={isShrink}>
+      <Stack direction={'row'} spacing={4}>
+        <Box mr={3}>
+          <img src="/images/sqa-logo.webp" alt="logo" />
         </Box>
-        <CustomButton variant={'contained'} width={130}>
-          <ShoppingCartIcon />
-        </CustomButton>
-      </Container>
-    </header>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/menu'}>Menu</Link>
+        <Link to={'/contact'}>Contact</Link>
+        <Link to={'/booking'}>Book a Table</Link>
+      </Stack>
+      <CustomButton variant={'contained'} width={130}>
+        <ShoppingCartIcon />
+      </CustomButton>
+    </Container>
   );
 };
 
-const Container = styled(Box)({
+const Container = styled(Box)(({ isShrink }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
+  zIndex: 9999,
+  margin: '0 auto',
+  transition: 'all 0.4s',
+  background: isShrink ? 'rgb(255,244,242)' : 'transparent',
+  boxShadow: isShrink ? '0 0 10px 8px rgba(0,0,0,0.1)' : 'none',
+  position: isShrink ? 'fixed' : 'relative',
+  top: 0,
   height: 80,
+  width: '100%',
+  padding: '0 120px',
   '& a': {
     position: 'relative',
     padding: '28px 5px',
-    margin: '0 5px',
     color: '#4d312c',
     fontWeight: 700,
     transition: 'all 0.4s ease-out',
@@ -60,4 +83,4 @@ const Container = styled(Box)({
       }
     }
   }
-});
+}));
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -22,6 +22,8 @@ export const HomePage = () => {
           ))}
         </Stack>
         <BookTable />
+        <BookTable />
+        <BookTable />
       </MainLayout>
     </>
   );
