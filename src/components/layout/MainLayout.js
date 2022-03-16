b93720a421525d19d47bb50d2fe7bddaa40a3c46
leminhthanh142 <thanhle12345789@gmail.com>
2022-03-17 00:38:46 +0700

    refactor

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Box } from '@mui/material';
 import PropTypes from 'prop-types';
 import { Header } from '../Header';
-import { HeroBackground } from '../HeroBackground';
 
 export const MainLayout = ({ children }) => {
   return (
-    <>
+    <Box>
       <Header />
-      <HeroBackground />
-      <Box>{children}</Box>
-    </>
+      {children}
+    </Box>
   );
 };
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,40 +5,39 @@ import { InformationCard } from '../components/InformationCard';
 import { Stack, Typography, Box, styled } from '@mui/material';
 import { BookTable } from '../components/bookTable/BookTable';
 import { PopularDishes } from '../components/PopularDishes';
+import { HeroBackground } from '../components/HeroBackground';
 
 export const HomePage = () => {
   return (
-    <>
-      <MainLayout>
-        <Wrapper mt={15}>
-          <Box mb={3}>
-            <Typography variant={'h2'} align={'center'}>
-              Best way to eat healthy food
-            </Typography>
-          </Box>
-          <Box mb={7}>
-            <Typography variant={'body2'} align={'center'}>
-              Making a reservation at Délicious restaurant is easy and takes just a couple of
-              minutes.
-            </Typography>
-          </Box>
-          <Stack direction={'row'} spacing={3} mb={10}>
-            {informationCard.map((card) => (
-              <InformationCard
-                key={card.id}
-                icon={card.icon}
-                title={card.title}
-                description={card.description}
-              />
-            ))}
-          </Stack>
-        </Wrapper>
-        <PopularDishes />
-        <Wrapper mt={15}>
-          <BookTable />
-        </Wrapper>
-      </MainLayout>
-    </>
+    <MainLayout>
+      <HeroBackground />
+      <Wrapper mt={15}>
+        <Box mb={3}>
+          <Typography variant={'h2'} align={'center'}>
+            Best way to eat healthy food
+          </Typography>
+        </Box>
+        <Box mb={7}>
+          <Typography variant={'body2'} align={'center'}>
+            Making a reservation at Délicious restaurant is easy and takes just a couple of minutes.
+          </Typography>
+        </Box>
+        <Stack direction={'row'} spacing={3} mb={10}>
+          {informationCard.map((card) => (
+            <InformationCard
+              key={card.id}
+              icon={card.icon}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
+        </Stack>
+      </Wrapper>
+      <PopularDishes />
+      <Wrapper mt={15}>
+        <BookTable />
+      </Wrapper>
+    </MainLayout>
   );
 };
 
